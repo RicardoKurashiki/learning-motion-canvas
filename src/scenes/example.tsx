b488d76ac54ce7ee7bb75876628fbd9b468ba43f
Ricardo Godoi Kurashiki <62195354+RicardoKurashiki@users.noreply.github.com>
2023-02-22 22:20:31 -0300

    Circle animation

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -1,8 +1,26 @@
-import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
-import {waitFor} from '@motion-canvas/core/lib/flow';
+import { Circle } from '@motion-canvas/2d/lib/components';
+import { makeScene2D } from '@motion-canvas/2d/lib/scenes';
+import { all } from '@motion-canvas/core/lib/flow';
+import { createSignal } from '@motion-canvas/core/lib/signals';
+import { createRef } from '@motion-canvas/core/lib/utils';
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
+  const circle = createRef<Circle>();
+  const scale = createSignal(1)
 
-  yield* waitFor(5);
+  view.add(
+    <Circle
+      ref={circle}
+      width={() => scale() * 100}
+      height={() => scale() * 100}
+      x={-300}
+      fill={'#ffffff'}
+    />
+  );
+
+  yield* all(
+    circle().position.x(-300, 0).to(300, 1),
+    scale(3, 0.8),
+  );
+  yield* circle().fill('#ffffff', 0).to('#A5E876', 1);
 });
